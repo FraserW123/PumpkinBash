c92c94d270c642ca4161b9b11c04e9a0f5859e45
Fraser <fgw@sfu.ca>
2022-10-15 15:02:44 -0700

    Setting up options screen

--- app/src/main/java/com/example/assignment3/OptionsScreen.java
+++ app/src/main/java/com/example/assignment3/OptionsScreen.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class OptionsScreen extends AppCompatActivity {
 
@@ -12,6 +17,80 @@ public class OptionsScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.options_screen);
+
+        createMinesRadioButtons();
+        createBoardSizeRadioButtons();
+        setSaveChanges();
+    }
+
+    private void createBoardSizeRadioButtons() {
+        RadioGroup group = findViewById(R.id.radio_board_size);
+        String [] boardOptions = getResources().getStringArray(R.array.board_size_options);
+
+        //create buttons
+        for(int i = 0; i<boardOptions.length; i++){
+            String  boardSize = boardOptions[i];
+
+            RadioButton button = new RadioButton(this);
+            button.setText(boardSize);
+
+            //TODO: Set on click callbacks
+            button.setOnClickListener(v -> Toast
+                    .makeText(OptionsScreen.this, "Board will be "+boardSize, Toast.LENGTH_SHORT)
+                    .show());
+
+            //Add to radio group
+            group.addView(button);
+        }
+    }
+
+    private void setSaveChanges() {
+        Button btn = findViewById(R.id.find_selected);
+        btn.setOnClickListener(v -> {
+            RadioGroup group_Mines = findViewById(R.id.radio_group_mines);
+            int idOfSelectedMines = group_Mines.getCheckedRadioButtonId();
+            RadioButton mineSelection = findViewById(idOfSelectedMines);
+
+
+            RadioGroup group_board_size = findViewById(R.id.radio_board_size);
+            int idOfSelectedBoardSize = group_board_size.getCheckedRadioButtonId();
+            RadioButton boardSelection = findViewById(idOfSelectedBoardSize);
+
+            if(mineSelection.getText() != null){
+                String message = mineSelection.getText().toString();
+                String message2 = boardSelection.getText().toString();
+                Toast.makeText(OptionsScreen.this, "Adding game configuration: " + message +
+                        " Board size: " + message2,
+                        Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(OptionsScreen.this, "No option have been selected",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+    }
+
+    private void createMinesRadioButtons() {
+        RadioGroup group = findViewById(R.id.radio_group_mines);
+        int [] num_mines = getResources().getIntArray(R.array.number_of_mines);
+
+        //create buttons
+        for(int i = 0; i<num_mines.length; i++){
+            int numMine = num_mines[i];
+
+            RadioButton button = new RadioButton(this);
+            button.setText(getString(R.string.mines, numMine));
+
+            //TODO: Set on click callbacks
+            button.setOnClickListener(v -> Toast
+                    .makeText(OptionsScreen.this, "You selected "+numMine, Toast.LENGTH_SHORT)
+                    .show());
+
+            //Add to radio group
+            group.addView(button);
+        }
     }
 
     public static Intent makeIntent(Context context){
---
+++ app/src/main/java/com/example/assignment3/model/Game.java
@@ -0,0 +1,166 @@
+package com.example.assignment3.model;
+import java.util.Random;
+
+public class Game {
+    int MAP_ROW ;
+    int MAP_COLUMN;
+    public int numMines;
+    boolean[] minePlaced;
+
+
+    public int[] map;
+    public int[] squareScores;
+
+    private int scans;
+    private int found = 0;
+    Random random = new Random();
+
+
+    public Game(int size, int mines){
+        setMapSize(size);
+        setNumMines(mines);
+
+        map = new int[MAP_COLUMN*MAP_ROW];
+        squareScores = new int[MAP_COLUMN*MAP_ROW];
+        minePlaced = new boolean[MAP_COLUMN*MAP_ROW];
+        setMap();
+
+    }
+
+    public int getBoardSize(){return MAP_COLUMN*MAP_ROW;}
+    public int getFound(){ return found; }
+    public int getNumScans(){ return scans; }
+
+    public void setMapSize(int choice){
+        switch (choice) {
+            case 1:
+                MAP_ROW = 4;
+                MAP_COLUMN = 6;
+                break;
+            case 2:
+                MAP_ROW = 5;
+                MAP_COLUMN = 10;
+                break;
+            case 3:
+                MAP_ROW = 6;
+                MAP_COLUMN = 15;
+                break;
+            default:
+                System.out.println("invalid choice defaulting to 4x6");
+                MAP_ROW = 4;
+                MAP_COLUMN = 6;
+                break;
+        }
+    }
+
+
+    public void setNumMines(int mines){
+
+        switch (mines) {
+            case 1:
+                numMines = 6;
+                break;
+            case 2:
+                numMines = 10;
+                break;
+            case 3:
+                numMines = 15;
+                break;
+            case 4:
+                numMines = 20;
+                break;
+            default:
+                System.out.println("Invalid choice defaulting to 6 mines");
+                numMines = 6;
+        }
+        //numMines = mines;
+
+
+
+    }
+
+    public void getBoard(){
+        System.out.println("board size = " + squareScores.length);
+        for(int i = 0; i<squareScores.length; i++){
+            if(i >= MAP_COLUMN && i%MAP_COLUMN == 0){
+                System.out.println();
+            }
+            if(map[i] != 0 && !minePlaced[i]){
+                System.out.print("* ");
+            }
+            else{
+                System.out.print(squareScores[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public void setMap(){
+
+        for(int i = 0; i<numMines; i++){
+            int setIndex = random.nextInt(MAP_COLUMN*MAP_ROW);
+            while(map[setIndex] != 0){
+                setIndex = random.nextInt(MAP_COLUMN*MAP_ROW);
+                //System.out.println("Repeat number replaced with " + setIndex);
+            }
+            //System.out.println("Placed mine at position " + setIndex);
+            minePlaced[setIndex] = true;
+            map[setIndex] = 1;
+        }
+    }
+
+    public boolean foundAllMines(){
+        return found == numMines;
+    }
+
+    public void checkMap(int index){
+        int count = 0;
+
+        if(index > 23){
+            System.out.println("Position set to zero since invalid");
+            index = 0;
+        }
+
+        int row = index;
+        int col = index;
+
+        if(index <=5){
+            row = 0;
+        }
+        else{
+            while(row%MAP_COLUMN != 0){
+                row -= 1;
+            }
+            while(col-MAP_COLUMN >= 0){
+                col -= MAP_COLUMN;
+            }
+        }
+
+        System.out.println("The index is in row " + row/MAP_COLUMN);
+        System.out.println("The index is in column " + col);
+        if(map[index] != 0 && minePlaced[index]){
+            System.out.println("Mine found on this square!");
+            minePlaced[index] = false;
+            found++;
+        }
+
+
+        // counting in the rows
+        for(int i = row; i<(row+6); i++){
+            if(map[i] != 0 && i!=index){
+                count++;
+            }
+        }
+        // counting in the columns
+        for(int i = col; i<map.length; i+=6){
+            if(map[i] != 0 && i!=index){
+                count++;
+            }
+        }
+
+        scans++;
+        squareScores[index] = count;
+        System.out.println("there are " + count + " mines near this square");
+
+    }
+}
