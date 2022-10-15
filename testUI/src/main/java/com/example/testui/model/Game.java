43be72b3347232a5cc169b15c265e65686689f57
Fraser <fgw@sfu.ca>
2022-10-15 13:09:37 -0700

    text logic implemented

--- testUI/src/main/java/com/example/testui/model/Game.java
+++ testUI/src/main/java/com/example/testui/model/Game.java
@@ -2,60 +2,109 @@ package com.example.testui.model;
 import java.util.Random;
 
 public class Game {
-    final int MAP_ROW = 3;
-    final int MAP_COLUMN = 5;
-    public int numMines = 2;
-    boolean[] mineFound;
+    int MAP_ROW ;
+    int MAP_COLUMN;
+    public int numMines;
+    boolean[] minePlaced;
+
+
     public int[] map;
     public int[] squareScores;
 
-    int found = 0;
+    private int scans;
+    private int found = 0;
     Random random = new Random();
 
 
-    public Game(){
-        mineFound = new boolean[]{true, true, true, true, true, true,
-                                true, true, true, true, true, true,
-                                true, true, true, true, true, true,
-                                true, true, true, true, true, true,};
+    public Game(int size, int mines){
+        setMapSize(size);
+        setNumMines(mines);
 
-        map = new int[]{0,0,0,0,0,0,
-                        0,0,0,0,0,0,
-                        0,0,0,0,0,0,
-                        0,0,0,0,0,0};
+        map = new int[MAP_COLUMN*MAP_ROW];
+        squareScores = new int[MAP_COLUMN*MAP_ROW];
+        minePlaced = new boolean[MAP_COLUMN*MAP_ROW];
+        setMap();
+
+    }
+
+    public int getBoardSize(){return MAP_COLUMN*MAP_ROW;}
+    public int getFound(){ return found; }
+    public int getNumScans(){ return scans; }
+
+    public void setMapSize(int choice){
+        switch (choice) {
+            case 1:
+                MAP_ROW = 4;
+                MAP_COLUMN = 6;
+                break;
+            case 2:
+                MAP_ROW = 5;
+                MAP_COLUMN = 10;
+                break;
+            case 3:
+                MAP_ROW = 6;
+                MAP_COLUMN = 15;
+                break;
+            default:
+                System.out.println("invalid choice defaulting to 4x6");
+                MAP_ROW = 4;
+                MAP_COLUMN = 6;
+                break;
+        }
+    }
 
 
-        squareScores = new int[]{0,0,0,0,0,0,
-                                 0,0,0,0,0,0,
-                                 0,0,0,0,0,0,
-                                 0,0,0,0,0,0};
+    public void setNumMines(int mines){
+
+        switch (mines) {
+            case 1:
+                numMines = 6;
+                break;
+            case 2:
+                numMines = 10;
+                break;
+            case 3:
+                numMines = 15;
+                break;
+            case 4:
+                numMines = 20;
+                break;
+            default:
+                System.out.println("Invalid choice defaulting to 6 mines");
+                numMines = 6;
+        }
+        //numMines = mines;
+
+
+
     }
 
     public void getBoard(){
+        System.out.println("board size = " + squareScores.length);
         for(int i = 0; i<squareScores.length; i++){
-            if(squareScores[i] == 1){
+            if(i >= MAP_COLUMN && i%MAP_COLUMN == 0){
+                System.out.println();
+            }
+            if(map[i] != 0 && !minePlaced[i]){
                 System.out.print("* ");
             }
             else{
                 System.out.print(squareScores[i] + " ");
             }
-            if(i == (MAP_ROW+1)){
-                System.out.println("");
-            }
-
         }
-        System.out.println("");
+        System.out.println();
     }
 
     public void setMap(){
 
         for(int i = 0; i<numMines; i++){
-            int setIndex = random.nextInt(6);
+            int setIndex = random.nextInt(MAP_COLUMN*MAP_ROW);
             while(map[setIndex] != 0){
-                setIndex = random.nextInt(6);
+                setIndex = random.nextInt(MAP_COLUMN*MAP_ROW);
+                //System.out.println("Repeat number replaced with " + setIndex);
             }
-            System.out.println("Placed mine at position " + setIndex);
-            mineFound[setIndex] = false;
+            //System.out.println("Placed mine at position " + setIndex);
+            minePlaced[setIndex] = true;
             map[setIndex] = 1;
         }
     }
@@ -66,17 +115,50 @@ public class Game {
 
     public void checkMap(int index){
         int count = 0;
-        if(map[index] != 0 && !mineFound[index]){
+
+        if(index > 23){
+            System.out.println("Position set to zero since invalid");
+            index = 0;
+        }
+
+        int row = index;
+        int col = index;
+
+        if(index <=5){
+            row = 0;
+        }
+        else{
+            while(row%MAP_COLUMN != 0){
+                row -= 1;
+            }
+            while(col-MAP_COLUMN >= 0){
+                col -= MAP_COLUMN;
+            }
+        }
+
+        System.out.println("The index is in row " + row/MAP_COLUMN);
+        System.out.println("The index is in column " + col);
+        if(map[index] != 0 && minePlaced[index]){
             System.out.println("Mine found on this square!");
-            mineFound[index] = true;
+            minePlaced[index] = false;
             found++;
         }
-        for(int i = 0; i<map.length; i++){
+
+
+        // counting in the rows
+        for(int i = row; i<(row+6); i++){
             if(map[i] != 0 && i!=index){
-                //System.out.println("found a mine at square " + i);
                 count++;
             }
         }
+        // counting in the columns
+        for(int i = col; i<map.length; i+=6){
+            if(map[i] != 0 && i!=index){
+                count++;
+            }
+        }
+
+        scans++;
         squareScores[index] = count;
         System.out.println("there are " + count + " mines near this square");
 
--- testUI/src/main/java/com/example/testui/tempMain.java
+++ testUI/src/main/java/com/example/testui/tempMain.java
@@ -3,25 +3,36 @@ package com.example.testui;
 import com.example.testui.model.Game;
 
 import java.util.Scanner;
-import java.util.Random;
 public class tempMain {
     public static void main(String[] args){
-        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-        Game game = new Game();
+        int choice;
 
+        System.out.println("What size of board?");
+        System.out.println("(1) 4x6");
+        System.out.println("(2) 5x10");
+        System.out.println("(3) 6x15");
+        choice = scanner.nextInt();
 
-        game.setMap();
+        System.out.println("How many mines to play with?");
+        System.out.println("(1) 6");
+        System.out.println("(2) 10");
+        System.out.println("(3) 15");
+        System.out.println("(4) 20");
+        int mines = scanner.nextInt();
+
+        Game game = new Game(choice, mines);
         while(!game.foundAllMines()){
-            System.out.println("Select a position (0-24)");
-            int choice = scanner.nextInt();
+            System.out.println("Select a position (" + 0 + "-" + game.getBoardSize()+")");
+            choice = scanner.nextInt();
             game.checkMap(choice);
             game.getBoard();
+            System.out.println("Scans: "+game.getNumScans());
+            System.out.println("Mines found " + game.getFound());
+
 
         }
         System.out.println("You found all the mines!");
 
-
-        //System.out.println("Hello world " + indexSet);
     }
 }
